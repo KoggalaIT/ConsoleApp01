39c33a3e9cdd521cf951d869dca5574c736ae333
nadun.kavindra@gmail.com <nadun.kavindra@gmail.com>
2019-03-08 11:27:21 +0530

    Removed

--- ConsoleApp01/Program.cs
+++ ConsoleApp01/Program.cs
@@ -12,21 +12,18 @@ namespace ConsoleApp01
         {
             Console.WriteLine("Hello GitHub");
             int x = 2;
+            if (true)
+            {
+                Console.WriteLine("Hello");
+            }
             while (x < 2)
             {
                 Console.WriteLine("Test");
             }
             Console.WriteLine("Sampath1");
             Console.WriteLine("Sampath");
-            if (true)
-            {
-                Console.WriteLine("Hello");
-            }
-            int v = 10;
-            if (true) Console.Write("Task Compleate");
-            Console.ReadLine();
-            if (true) Console.Write("Task Compleate");
-            if (true) Console.Write("Task Compleate");
+            
+
         }
     }
 }
